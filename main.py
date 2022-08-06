bc237e95b82960edd0ccd3f2f99651392bcd0fca
SVingelsgaard <sigurd@vingelsgaard.no>
2022-08-06 15:15:21 +0200

    graphics shit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import cv2 as cv
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg") # set the backend  
 
 
 mnist = tf.keras.datasets.mnist
@@ -11,9 +13,11 @@ mnist = tf.keras.datasets.mnist
 
 xTrain = xTrain.astype('float32')
 xTest = xTest.astype('float32')
-
+'''
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape = (28, 28)),
+    tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
     tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
     tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
     tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)
@@ -23,11 +27,25 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy', 
               metrics='accuracy')
 
-model.fit(xTrain, yTrain, epochs=10)
+model.fit(xTrain, yTrain, epochs=25)
 
 loss, accuracy = model.evaluate(xTest, yTest)
 
 model.save('digits.model')
+'''
+model = tf.keras.models.load_model('digits.model')
+
+def move_figure(f, x, y):
+    """Move figure's upper left corner to pixel (x, y)"""
+    backend = matplotlib.get_backend()
+    if backend == 'TkAgg':
+        f.canvas.manager.window.wm_geometry("+%d+%d" % (x, y))
+    elif backend == 'WXAgg':
+        f.canvas.manager.window.SetPosition((x, y))
+    else:
+        # This works for QT and GTK
+        # You can also use window.setGeometry
+        f.canvas.manager.window.move(x, y)
 
 for i in range(10):
 
@@ -38,6 +56,10 @@ for i in range(10):
 
     #plotin
     plt.imshow(imgForGraphics, cmap=plt.cm.binary)
+    thismanager = plt.get_current_fig_manager()
+    thismanager.window.wm_geometry("+100+100")
     plt.show()
     plt.bar(np.arange(len(prediction.ravel())), prediction.ravel())
+    thismanager = plt.get_current_fig_manager()
+    thismanager.window.wm_geometry("+100+100")
     plt.show()
